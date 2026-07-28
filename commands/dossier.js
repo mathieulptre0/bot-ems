e6d878929e879954aa9bf8b3cc11b63e4a19252a
mathieulptre0 <mat.lptre0@gmail.com>
2026-07-28 23:54:31 +0200

    Mise à jour du bot

--- commands/dossier.js
+++ commands/dossier.js
@@ -153,7 +153,7 @@ module.exports = {
                 .setRequired(true);
 
             const fileLabel = new LabelBuilder()
-                .setLabel('Pièce jointe')
+                .setLabel('Pièce Identité')
                 .setFileUploadComponent(fileUploadComponent);
 
             modal.addComponents(
